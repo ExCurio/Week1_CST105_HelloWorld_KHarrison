b5a38f645bdcaf2c819059af546d2d8a3f2900a8
Keith Harrison <ex.curiositatem.scientia@gmail.com>
2018-02-12 19:55:18 -0700

    Hello World Program.

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/** Program: Hello World Program
+ * File: HelloWorld.java
+ * Summary: This program prints 'Hello World!' to the console.
+ *
+ * Author: Keith Harrison
+ * Date: 2/12/2018
+ * 
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Print 'Hello World!' to the console.
+        System.out.println("Hello World!");
+    }
+    
+}
